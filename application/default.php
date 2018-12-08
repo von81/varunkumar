490898d5a8fea3ae9a18165bdba6702af5321e83
von81 <doervarun@gmail.com>
2018-12-09 02:43:58 +0530

    First Commit

---
+++ application/default.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+  <title>Varun Kumar : Technology Enthusiast</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+  html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+  .text-white{color: #fff;}
+  </style>
+  <body class="w3-light-grey">
+    <!-- Page Container -->
+    <div class="w3-content w3-margin-top" style="max-width:1400px;">
+      <!-- The Grid -->
+      <div class="w3-row-padding">
+        
+        <!-- Left Column -->
+        <div class="w3-third">
+          
+          <div class="w3-white w3-text-grey w3-card-4">
+            <div class="w3-display-container">
+              <img src="varun_kumar.jpg" style="width:100%" alt="Varun kumar">
+              <div class="w3-display-bottomleft w3-container w3-text-black">
+                <h2 class="text-white">Varun Kumar</h2>
+              </div>
+            </div>
+            <div class="w3-container">
+              <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Software Engineers</p>
+              <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Delhi, India</p>
+              <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>doervarun@gmail.com</p>
+              <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>+91 9711532136</p>
+              <hr>
+              <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+              <p>PHP</p>
+              <div class="w3-progress-container w3-round-xlarge w3-small">
+                <div class="w3-progressbar w3-round-xlarge w3-teal" style="width:85%">
+                  <div class="w3-center w3-text-white">95%</div>
+                </div>
+              </div>
+              <p>Mysql</p>
+              <div class="w3-progress-container w3-round-xlarge w3-small">
+                <div class="w3-progressbar w3-round-xlarge w3-teal" style="width:80%">
+                  <div class="w3-center w3-text-white">90%</div>
+                </div>
+              </div>
+              <p>JavaScript</p>
+              <div class="w3-progress-container w3-round-xlarge w3-small">
+                <div class="w3-progressbar w3-round-xlarge w3-teal" style="width:75%">
+                  <div class="w3-center w3-text-white">75%</div>
+                </div>
+              </div>
+              <p>GIT</p>
+              <div class="w3-progress-container w3-round-xlarge w3-small">
+                <div class="w3-progressbar w3-round-xlarge w3-teal" style="width:50%">
+                  <div class="w3-center w3-text-white">70%</div>
+                </div>
+              </div>
+              <p>Linux</p>
+              <div class="w3-progress-container w3-round-xlarge w3-small">
+                <div class="w3-progressbar w3-round-xlarge w3-teal" style="width:60%">
+                  <div class="w3-center w3-text-white">70%</div>
+                </div>
+              </div>
+              <br>
+              <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+              <p>English</p>
+              <div class="w3-progress-container w3-round-xlarge">
+                <div class="w3-progressbar w3-round-xlarge w3-teal" style="width:100%"></div>
+              </div>
+              <p>Hindi</p>
+              <div class="w3-progress-container w3-round-xlarge">
+                <div class="w3-progressbar w3-round-xlarge w3-teal" style="width:55%"></div>
+              </div>
+              <br>
+            </div>
+          </div><br>
+          <!-- End Left Column -->
+        </div>
+        <!-- Right Column -->
+        <div class="w3-twothird">
+          
+          <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Sr. Software Engineer / Oxigen Wallet from Unifytech Pvt. Ltd. </b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2<sup>nd</sup> Nov 2015 to <sup>th</sup> 2016</h6>
+              <hr>
+<!--              <ul>
+                <li>UI development using several advance development tools
+                </li>
+              </ul>
+-->
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Senior Software Engineer / Afycon Technology Pvt. Ltd.</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>August, 2013 to <sup>th</sup> Oct, 2015 <span class="w3-tag w3-teal w3-round">Current</span></h6>
+              <hr>
+<!--              <ul>
+                <li>Responsibility for the company’s projects delivery.
+                </li>
+                <li>Responsibility for the motivation and development of 8 members of staff
+                </li>
+                <li>Responsibility of creating MOU for onboard Project.
+                </li>
+              </ul> -->
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Software Engineer / Mydala.com<Kinobeo Software Pvt. Ltd.> </b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>19<sup>th</sup> Jul,2012 to Jul 2013</h6>
+              <hr>
+<!--              <ul>
+                <li>UI development using several advance development tools
+                </li>
+                <li>PSD’s designing
+                </li>
+                <li>Responsibility of slicing html of designs
+                </li>
+                <li>Making web site compatible with mobile and iPad devices
+                </li>
+              </ul>
+-->
+            </div>
+          </div>
+          <div class="w3-container w3-card-2 w3-white">
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>B.Tech (Computer Science)</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2012 - 2013</h6>
+              <p>Govt. Engineering College, Bharatpur</p>
+              <p>Rajasthan Technical University, Kota<p>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>12th</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2014 - 2016</h6>
+              <p>Rajasthan Board, Ajmer</p>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>10th</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2011 - 2014</h6>
+              <p>Rajasthan Board, Ajmer</p>
+             <hr> 
+            </div>
+          </div>
+          <!-- End Right Column -->
+        </div>
+        
+        <!-- End Grid -->
+      </div>
+      
+      <!-- End Page Container -->
+    </div>
+    <footer class="w3-container w3-teal w3-center w3-margin-top">
+      <p>Find me on social media.</p>
+      <a href="javascript:void(0);" target="_blank"><i class="fa fa-facebook-official w3-hover-text-indigo w3-large"></i></a> &nbsp; 
+      <a href="javascript:void(0);" target="_blank"><i class="fa fa-instagram w3-hover-text-purple w3-large"></i></a>&nbsp; 
+      <a href="javascript:void(0);" target="_blank"><i class="fa fa-pinterest-p w3-hover-text-red w3-large"></i></a>&nbsp; 
+      <a href="javascript:void(0);" target="_blank"><i class="fa fa-twitter w3-hover-text-light-blue w3-large"></i></a>&nbsp; 
+      <a href="javascript:void(0);" target="_blank"><i class="fa fa-linkedin w3-hover-text-indigo w3-large"></i></a>
+      <br>
+      <br>
+    </footer>
+  </body>
+</html>		
